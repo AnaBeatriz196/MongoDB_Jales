ab4361ff24d532c5f4f6fc497049554ba7c3aa68
Jales Monteiro <monteiro_iale@hotmail.com>
2025-06-18 14:05:53 +0000

    v1, apenas teste de conexão

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mongodb
+
+curl ifconfig.me
---
+++ teste.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
+
+# Obtém a URI do MongoDB a partir do .env
+uri = os.getenv('MONGO_URI')
+
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+try:
+    client.admin.command('ping')
+    print("Ping! Você se conectou com sucesso ao MongoDB!")
+except Exception as e:
+    print(f"Erro ao conectar: {e}")
